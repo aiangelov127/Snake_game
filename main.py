3673b3a7d65340751d194df50eade7429a9325c4
aiangelov127 <aiangelov127@gmail.com>
2021-11-02 17:21:09 +0200

    The classic snake
    The classic arcade snake game, visualized with turtle graphics.

---
+++ main.py
@@ -0,0 +1,50 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Welcome to the snake game")
+screen.tracer(0)  # method to allow for update of the animation
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+
+screen.listen()
+
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        snake.extend()
+        food.refresh()
+        scoreboard.keep_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_end()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_end()
+
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVEMENT_STEP = 20
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.snake_position()
+        self.head = self.segments[0]
+
+    def snake_position(self):  # creates a snake
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_s = Turtle("square")
+        new_s.penup()
+        new_s.color("white", "white")
+        new_s.goto(position)
+        self.segments.append(new_s)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):  # Snake constant movement and following of segments
+        for s_num in range(len(self.segments) - 1, 0, -1):  # (start=2 or len() , stop=0, step=-1) to start from back
+            new_x = self.segments[s_num - 1].xcor()
+            new_y = self.segments[s_num - 1].ycor()
+            self.segments[s_num].goto(new_x, new_y)
+        self.head.fd(MOVEMENT_STEP)
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
